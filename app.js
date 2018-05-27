d7621b9c212853a8376b6c4b2c6a63d431e36f20
mariajosesalazar <mariaj.salazarf@gmail.com>
2018-05-27 18:46:47 -0400

    funciones definidas, se ejecuta correctamente solo la suma

---
+++ app.js
@@ -0,0 +1,18 @@
+//Crea dos funciones, add y multiply. Cada una debe tomar dos argumentos. 
+//La función add debe sumar sus argumentos y devolver el resultado. 
+const add = (number2, number3) => number2 + number3;
+//La funcion multiply debe multiplicar sus argumentos. 
+const multiply = (add, number1) => add * number1;
+//Usando solamente estas dos funciones, resuelva este simple problema matemático: 36325 * (9824 + 777).
+
+//let number1 = 36325;
+//let number2 = 9824;
+//let number3 = 777;
+
+console.log(add(9842, 777));
+
+console.log(multiply(36325, add));
+
+
+
+
